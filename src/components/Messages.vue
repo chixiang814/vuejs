04047d8c2d5f5b873fac9757466c664b350f940c
CONFIGURA\chiya <chixiang.yap@configura.com>
2022-04-20 21:03:14 +0800

    update after read doc

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -2,8 +2,8 @@
     <div id="messageContainer" class="overflow-auto h-72">
             <div class="flex m-1 mt-3" v-for="(item) in messages" :key="item">
                 <img src="../assets/ppl.jpg" alt="" v-show="!item.self" class="rounded-full mt-1 ml-2 h-8 w-8 object-cover">
-                <div class="bg-slate-600 w-fit h-fit p-1 rounded-md ml-2 cursor-pointer" v-show="!item.self">{{item.message}}</div>
-                <div class="bg-slate-700 w-fit h-fit p-1 rounded-md ml-auto cursor-pointer" v-show="item.self">{{item.message}}</div>
+                <div v-if="!item.self" class="bg-slate-600 w-fit h-fit p-1 rounded-md ml-2 cursor-pointer">{{item.message}}</div>
+                <div v-else class="bg-slate-700 w-fit h-fit p-1 rounded-md ml-auto cursor-pointer">{{item.message}}</div>
             </div>
     </div>
 </template>
